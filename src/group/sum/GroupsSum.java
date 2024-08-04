9fa7827cb49d29e83db0ea7cac54c63237bb7dab
user.emial <emil.mehraliyev003@gmail.com>
2024-08-05 00:42:58 +0300

    HW 42 completed

---
+++ src/group/sum/GroupsSum.java
@@ -0,0 +1,23 @@
+package group.sum;
+
+import java.util.Arrays;
+
+abstract public class GroupsSum {
+	protected int[][] groups;
+
+	public GroupsSum(int[][] groups) {
+		super();
+		this.groups = groups;
+	}
+
+	protected SumArrayTask[] getTasks() {
+		return Arrays.stream(groups).map(a -> new SumArrayTask(a)).toArray(SumArrayTask[]::new);
+	}
+
+	protected long sumOfGroups(SumArrayTask[] tasks) {
+
+		return Arrays.stream(tasks).mapToLong(SumArrayTask::getResult).sum();
+	}
+
+	abstract public long getSum();
+}
---
+++ src/group/test/GroupsSumTest.java
@@ -0,0 +1,50 @@
+package group.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Random;
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.Test;
+
+import group.sum.GroupsSum;
+import group.sum.GroupsSumTaskThread;
+import group.sum.GroupsSumThreadPool;
+
+class GroupsSumTest {
+private static final int N_GROUPS = 1000;
+private static final int GROUP_LENGTH = 1000;
+int[][] groups = {
+		{1,2},
+		{3,4},
+		{5,6}
+};
+int[][] largeGroups = getLargeGroups(N_GROUPS, GROUP_LENGTH);
+	@Test
+	void goupsSumTaskThreadTest() {
+		GroupsSum gs = new GroupsSumTaskThread(groups);
+		assertEquals(21, gs.getSum());
+	}
+	private int[][] getLargeGroups(int nGroups, int groupLength) {
+		// DONE creating random two dimensional array 
+		// using method generate of Stream
+		Random random = new Random();
+		return IntStream.range(0, nGroups)
+				.mapToObj(i -> random.ints(groupLength,1,100).toArray())
+		        .toArray(int[][]::new);
+	}
+	@Test
+	void goupsSumThreadPoolTest() {
+		GroupsSum gs = new GroupsSumThreadPool(groups);
+		assertEquals(21, gs.getSum());
+	}
+	@Test
+	void groupsSumTaskThreadPerformance() {
+		new GroupsSumTaskThread(largeGroups).getSum();
+	}
+	@Test
+	void groupsSumTaskThreadPoolsPerformance() {
+		new GroupsSumThreadPool(largeGroups).getSum();
+	}
+
+}
